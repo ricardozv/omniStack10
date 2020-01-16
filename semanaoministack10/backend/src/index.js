cdcd0d73abada080aee3a06d258082dcd6575237
ricozv <ricardosilvanet03@gmail.com>
2020-01-16 17:59:09 -0300

    moving index,js

---
+++ semanaoministack10/backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://rico:rico1983@cluster0-jvcde.mongodb.net/geo?retryWrites=true&w=majority', {
+    useNewUrlParser: true,  
+    useUnifiedTopology: true});
+
+app.use(express.json());
+
+
+
+app.listen(3333);
