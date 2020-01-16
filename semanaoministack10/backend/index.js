84d3478ce04e94e28998a8094909d065cca1ead7
ricozv <ricardosilvanet03@gmail.com>
2020-01-16 17:43:46 -0300

    conect in data base

--- semanaoministack10/backend/index.js
+++ semanaoministack10/backend/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
+mongoose.connect('mongodb+srv://rico:rico1983@cluster0-jvcde.mongodb.net/geo?retryWrites=true&w=majority', {
+    useNewUrlParser: true,  
+    useUnifiedTopology: true});
+
+app.use(express.json());
+
 app.get('/', (request, Response) => {
-    return Response.send('Servidor rodando na bagaceira!')
+    return Response.json({ message:'Servidor rodando na bagaceira do sertão!'})
 });
 
 app.listen(3333);
