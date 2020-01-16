2961890a9aba6b1987f5b86e6c668f71d193e628
ricozv <ricardosilvanet03@gmail.com>
2020-01-16 20:21:33 -0300

    api github

--- semanaoministack10/backend/src/routes.js
+++ semanaoministack10/backend/src/routes.js
@@ -1,14 +1,27 @@
 const { Router} = require('express');
 const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
 routes.post('/devs', async (request, response) => {
-    const { github_username } = request.body;
+    const { github_username, techs } = request.body;
 
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
-    console.log(apiResponse.data);
+    const { name = login, avatar_url, bio } = apiResponse.data;
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+    })
+
+    console.log(name, avatar_url, bio, github_username);
 
     return response.json({ message:'Servidor rodando na bagaceira do sertão!'});
 });
