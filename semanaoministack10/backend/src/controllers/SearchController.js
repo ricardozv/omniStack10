0b9f41249411d5817811964d887a406616670628
ricozv <ricardosilvanet03@gmail.com>
2020-01-17 01:10:19 -0300

    controllers

---
+++ semanaoministack10/backend/src/controllers/SearchController.js
@@ -0,0 +1,14 @@
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index(request, response) {
+        const { latitude, longitude, techs } = request.query;
+
+        const techsArray = parseStringAsArray(techs);
+ 
+        console.log(techsArray);
+
+        return response.json({ devs: [] });
+    }
+}
